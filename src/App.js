e2ac8562a82f1002800df033cb2d0bcf1f29ee2f
musabkhan <musabkhan@t7e.in>
2023-01-12 21:39:20 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import AppRouter from './route/AppRouter';
 import Navbar from './components/Navbar';
@@ -9,21 +8,6 @@ function App() {
       <Navbar />
       
       <AppRouter/>
-      
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
     </div>
   );
 }
